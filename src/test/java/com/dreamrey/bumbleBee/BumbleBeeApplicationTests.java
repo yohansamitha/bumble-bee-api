25fe1a5e9c89aae95a8207b5671a3fb410fb551d
YohanSamitha <yohansamitha123@gmail.com>
2023-04-01 23:22:21 +0530

    initial commit

---
+++ src/test/java/com/dreamrey/bumbleBee/BumbleBeeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dreamrey.bumbleBee;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BumbleBeeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
